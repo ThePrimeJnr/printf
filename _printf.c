5a7ccf75a9bf89beec1dc4979531c08233a3dc0a
DestinedCodes <yiradesat@gmail.com>
2023-06-24 21:26:47 +0100

    Add basic Printf (untested)

---
+++ _printf.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _printf - Prints to the standar output like regular printf
+ * @format: The string to be printed adn its format specifiers
+ *
+ * Return: Length of printed string
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0;
+
+	while (format && format[i])
+		_putchar(format[i]);
+
+	_putchar('\n');
+
+	return (i);
+}
+
