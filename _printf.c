f8efabeb1df8f6c6b62e2544f57482037813861e
Thuba <ttshehlane@outlook.com>
2023-06-24 17:08:55 -0700

    added the updated printf

--- _printf.c
+++ _printf.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * _printf - Prints to the standar output like regular printf
@@ -8,16 +11,42 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
+	va_list ls_args;
+	int i = 0, c;
+	char *str_temp;
 
+	va_start(ls_args, format);
 	while (format && format[i])
 	{
-		_putchar(format[i]);
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					_putchar(va_arg(ls_args, int));
+					break;
+				case 's':
+					str_temp = va_arg(ls_args, char *);
+					if (str_temp != NULL)
+					{
+						c += _putstr(str_temp);
+						break;
+					}
+					c += _putstr("(Null)");
+					break;
+				default:
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			c++;
+		}	
 		i++;
 	}
-
+	va_end(ls_args);
 	_putchar('\0');
-
 	return (i);
 }
-
