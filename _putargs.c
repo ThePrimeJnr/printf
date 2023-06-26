a03cf0ac42d76fbe57b3236cac57d3c9eb7e94b5
Thuba <ttshehlane@outlook.com>
2023-06-26 14:44:48 -0700

    edit putbit

--- _putargs.c
+++ _putargs.c
@@ -87,11 +87,11 @@ int _putbit(unsigned int num)
 		len += _putchar('0');
 		return (len);
 	}
-	if (num < 0)
+	/*if (num < 0)
 	{
 		for (val = 1 << 31; val > 0; val /= 2)
 			(num & val) ? _putchar('1') : _putchar('0');
-	}
+	}*/
 
 	val = num % 2;
 	num = num / 2;
