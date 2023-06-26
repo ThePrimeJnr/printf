b69dbd1d796a0ecd4f4efbb52abdaafecc6bcf36
Thuba <ttshehlane@outlook.com>
2023-06-26 14:34:21 -0700

    fixed the prototype of putbit

--- main.h
+++ main.h
@@ -11,7 +11,7 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int _putstr(char *str);
 int _putint(int num);
-int _putbit(int num);
+int _putbit(unsigned int num);
 int _spec_handler (va_list ls_args, char spec);
 
 #endif /* PRINTF_H */
